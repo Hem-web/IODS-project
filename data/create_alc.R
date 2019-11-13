76f1c11442f48c9f9d195f60f719aa61bfec3e48
Hem Bhattarai <hemb@uef.fi>
2019-11-13 15:44:01 +0200

    progressing

--- data/create_alc.R
+++ data/create_alc.R
@@ -1,18 +1,26 @@
 #Name:Hem Raj Bhattarai
 #Date:13.11.2019
-#Comment on file: The downloaded files are in comma seprated files. 
+#Comment on file: The downloaded files are as comma seprated files. 
 
 #Setting working directory
 setwd("C:/Users/hemb/Documents/IODS-project/data")
 getwd()
 
 #reading the data
-studentmat<-read.csv("student-mat.csv", header = TRUE)
-studentpor<-read.csv("student-por.csv", header = TRUE)
+studentmat<-read.table("student-mat.csv", sep = ";", header = TRUE)
+studentpor<-read.table("student-por.csv", sep = ";", header = TRUE)
 
 #Looking into the data dimensions and strucutre
 dim(studentmat)
 dim(studentpor)
 str(studentmat)
 str(studentpor)
+colnames(studentmat)
+colnames(studentpor)
+
+#Comment on the data dimensions and structures: 
+
+#installing package
+library(rlang)
+library(dplyr)
 
