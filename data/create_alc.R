8be82f42bf388e2c690d7d05aa5d072cd0ab20a1
Hem Bhattarai <hemb@uef.fi>
2019-11-16 22:38:09 +0200

    working

--- data/create_alc.R
+++ data/create_alc.R
@@ -7,20 +7,33 @@ setwd("C:/Users/hemb/Documents/IODS-project/data")
 getwd()
 
 #reading the data
-studentmat<-read.table("student-mat.csv", sep = ";", header = TRUE)
-studentpor<-read.table("student-por.csv", sep = ";", header = TRUE)
+math<-read.table("student-mat.csv", sep = ";", header = TRUE)
+por<-read.table("student-por.csv", sep = ";", header = TRUE)
 
 #Looking into the data dimensions and strucutre
-dim(studentmat)
-dim(studentpor)
-str(studentmat)
-str(studentpor)
-colnames(studentmat)
-colnames(studentpor)
+dim(math)
+dim(por)
+str(math)
+str(por)
+colnames(math)
+colnames(por)
 
 #Comment on the data dimensions and structures: 
 
 #installing package
-library(rlang)
+
 library(dplyr)
+
+# common columns to use as identifiers
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+
+# join the two datasets by the selected identifiers
+math_por <- inner_join(math, por, by = "school")
+
+# see the new column names
+colnames(math_por)
+
+# glimpse at the data
+glimpse(math_por)
+
 
