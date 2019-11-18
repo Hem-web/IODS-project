80936136b17bda6d82540a0eed3289cda371aecc
Hem Bhattarai <hemb@uef.fi>
2019-11-18 18:21:47 +0200

    data is rechecked

--- data/create_alc.R
+++ data/create_alc.R
@@ -26,58 +26,60 @@ colnames(por)
 # on two subjects, mathematics and portugese language in two different portugese schools. There were
 #395 observations on  those variables for mathematics and 649 observations for portugese language.
 
-
-#installing package
-
-library(dplyr)
-
 #4# Joining two data sets:
+library(dplyr)
 #Identifying the common columns in both data frame
-join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+# Define own id for both datasets
 
-# join the two datasets by the selected identifiers
-math_por <- inner_join(math, por, by = join_by)
-colnames(math_por)
+por_id <- por %>% mutate(id=1000+row_number()) 
+math_id <- math %>% mutate(id=2000+row_number())
+
+# Which columns vary in datasets
+free_cols <- c("id","failures","paid","absences","G1","G2","G3")
+
+# The rest of the columns are common identifiers used for joining the datasets
+join_cols <- setdiff(colnames(por_id),free_cols)
+
+pormath_free <- por_id %>% bind_rows(math_id) %>% select(one_of(free_cols))
+
+# Combine datasets to one long data
+pormath <- por_id %>% 
+  bind_rows(math_id) %>%
+  # Aggregate data (more joining variables than in the example)  
+  group_by(.dots=join_cols) %>%  
+  # Calculating required variables from two obs  
+  summarise(                                                           
+    n=n(),
+    id.p=min(id),
+    id.m=max(id),
+    failures=round(mean(failures)),     #  Rounded mean for numerical
+    paid=first(paid),                   #    and first for chars
+    absences=round(mean(absences)),
+    G1=round(mean(G1)),
+    G2=round(mean(G2)),
+    G3=round(mean(G3))    
+  ) %>%
+  # Remove lines that do not have exactly one obs from both datasets
+  #   There must be exactly 2 observations found in order to joining be succesful
+  #   In addition, 2 obs to be joined must be 1 from por and 1 from math
+  #     (id:s differ more than max within one dataset (649 here))
+  filter(n==2, id.m-id.p>650) %>%  
+  # Join original free fields, because rounded means or first values may not be relevant
+  inner_join(pormath_free,by=c("id.p"="id"),suffix=c("",".p")) %>%
+  inner_join(pormath_free,by=c("id.m"="id"),suffix=c("",".m")) %>%
+  # Calculate other required variables  
+  ungroup %>% mutate(
+    alc_use = (Dalc + Walc) / 2,
+    high_use = alc_use > 2,
+    cid=3000+row_number()
+  )
+
 
 # structure and dimension or glimpse of the combined data the data
-str(math_por)
-dim(math_por)
-colnames(math_por)
+str(pormath)
+dim(pormath)
+colnames(pormath)
 
-
-## Joining the duplicated answers by using if-else function
-alc <- math_por %>% select(one_of(join_by))
-colnames(alc)
-
-for(column_name in notjoined_columns) {
-  # select two columns from 'math_por' with the same original name
-  two_columns <- select(math_por, starts_with(column_name))
-  # select the first column vector of those two columns
-  first_column <- select(two_columns, 1)[[1]]
-  
-  # if that first column vector is numeric...
-  if(is.numeric(first_column)) {
-    # take a rounded average of each row of the two columns and
-    # add the resulting vector to the alc data frame
-    alc[column_name] <- round(rowMeans(two_columns))
-  } else { # else if it's not numeric...
-    # add the first column vector to the alc data frame
-    alc[column_name] <- first_column}}
-
-
-## Data mutation
- library(ggplot2)
-
-# This function is creating a new column name "alc_use" by averaging daily and weekly alchol consumption
-# and also creating a logical column "high_use" be defining use of alchol or "alc_use" which is greater than 2.
-alc <- alc %>% mutate (alc_use = (Dalc+Walc)/2, high_use = (alc_use > 2))
-colnames(alc)
-glimpse (alc)
-
-# For bar graphs visulaization of alcohol use by sex
-g1 <- ggplot(data = alc, aes(x = alc_use))
-g1 + geom_bar()
-g1 + geom_bar()+ facet_wrap("sex")
 
 # Setting Wroking directory
 setwd("~/IODS-project")
@@ -85,14 +87,13 @@ setwd("~/IODS-project")
 
 #Save created data to folder 'data' as a comma seperated (CSV) file
 library(csvread)
-write.csv(alc,file="~/IODS-project/data/alc.csv")
-readtest <- read.table("~/IODS-project/data/alc.csv")
+write.csv(alc,file="~/IODS-project/data/pormath.csv")
+readtest <- read.table("~/IODS-project/data/pormath.csv")
 readtest
 #Save created data to folder 'data' as a Table/text
-write.table(alc,file="~/IODS-project/data/alc.txt")
-readtest1 <- read.table("~/IODS-project/data/alc.txt")
+write.table(alc,file="~/IODS-project/data/pormath.txt")
+readtest1 <- read.table("~/IODS-project/data/pormath.txt")
 readtest1
-
 
 
 
